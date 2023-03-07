3c57342101750583060c90227a9151cca5a1d90c
dingdangmaoup <dingdangmaoup@gmail>
2023-03-07 10:08:34 +0800

    style(fourd): reformate
    1,reformate

--- fourd/fourd-bootstrap/src/main/java/com/dingdangmaoup/fourd/bootstrap/BootstrapApplication.java
+++ fourd/fourd-bootstrap/src/main/java/com/dingdangmaoup/fourd/bootstrap/BootstrapApplication.java
@@ -1,8 +1,10 @@
 package com.dingdangmaoup.fourd.bootstrap;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication(scanBasePackages = {"com.dingdangmaoup.fourd", "com.alibaba.cola"})
 public class BootstrapApplication {
@@ -11,5 +13,6 @@ public class BootstrapApplication {
   public static void main(String[] args) {
     SpringApplication.run(BootstrapApplication.class, args);
   }
+
 
 }
