5aecd21fc696d881e11631b63fddd9ee0dcffd67
dingdangmaoup <dingdangmaoup@gmail>
2023-03-07 09:18:24 +0800

    style(openai): test
    1,数据脱敏

--- fourd/fourd-bootstrap/src/test/java/com/dingdangmaoup/fourd/bootstrap/BootstrapApplicationTest.java
+++ fourd/fourd-bootstrap/src/test/java/com/dingdangmaoup/fourd/bootstrap/BootstrapApplicationTest.java
@@ -1,16 +1,12 @@
 package com.dingdangmaoup.fourd.bootstrap;
 
 
-import com.dingdangmaoup.openai.config.OpenaiProperties;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootstrapApplicationTest {
 
-  @Autowired
-  OpenaiProperties openaiProperties;
 
   @Test
   void contextLoads() {
